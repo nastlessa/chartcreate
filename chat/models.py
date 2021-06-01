6753efbc15adc5a5c282ffbc41ed7a0b69f16e57
nastlessa <nastlessa888@gmail.com>
2021-06-01 17:56:59 +0300

    chatcreate

---
+++ chat/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from datetime import datetime
+
+# Create your models here.
+class Room(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=1000)
+class Message(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    value = models.CharField(max_length=1000000)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    user = models.CharField(max_length=1000000)
+    room = models.CharField(max_length=1000000)
